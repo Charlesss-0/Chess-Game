9d3e1eb6a0d332fa075e4d06684f4d9cd77837d3
Carlos505 <aragonaugusto24@gmail.com>
2023-10-18 13:12:24 -0600

    Redesign code and replace chess pieces

--- src/index.js
+++ src/index.js
@@ -1,6 +1,3 @@
-import { placeInitialPieces } from './pieces' // Function to place the initial chess pieces on the board
-import { DragEvents } from './pieces' // Function to set event listeners on the squares
-
 const chessboard = [
     ['r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'],
     ['p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'],
@@ -12,39 +9,128 @@ const chessboard = [
     ['R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'],
 ]
 
-// Create the chess board grid
-createGrid(8)
-
-// Create grid board and place initial chess pieces
-function createGrid(n) {
+function displayBoard() {
     const board = document.getElementById('board')
-    board.classList.add('grid', 'board-border')
 
-    // Create the grid squares
-    for (let i = 0; i < n; i++) {
-        for (let j = 0; j < n; j++) {
-            const square = createSquares(i, j, n)
-            square.classList.add((i + j) % 2 === 0 ? 'beige' : 'dark')
+    handleDragDropEvents(board)
+
+    chessboard.forEach((row, rowIndex) => {
+        row.forEach((piece, columnIndex) => {
+            const square = document.createElement('div')
+            square.classList.add(
+                'square',
+                'w-24',
+                'h-24',
+                rowIndex % 2 === columnIndex % 2 ? 'bg-gray-300' : 'bg-gray-500'
+            )
+
+            const king = 'fi fi-sr-chess-king'
+            const queen = 'fi fi-sr-chess-queen'
+            const rook = 'fi fi-sr-chess-rook-alt'
+            const knight = 'fi fi-sr-chess-knight-alt'
+            const bishop = 'fi fi-sr-chess-bishop'
+            const pawn = 'fi fi-sr-chess-pawn-alt'
+
+            const isWhitePiece = piece === piece.toUpperCase()
+            const whiteBlack = isWhitePiece ? 'text-white' : 'text-black'
+
+            switch (true) {
+                case piece === 'R':
+                    square.innerHTML += getChessPiece(rook, whiteBlack)
+                    break
+                case piece === 'N':
+                    square.innerHTML += getChessPiece(knight, whiteBlack)
+                    break
+                case piece === 'B':
+                    square.innerHTML += getChessPiece(bishop, whiteBlack)
+                    break
+                case piece === 'Q':
+                    square.innerHTML += getChessPiece(queen, whiteBlack)
+                    break
+                case piece === 'K':
+                    square.innerHTML += getChessPiece(king, whiteBlack)
+                    break
+                case piece === 'P':
+                    square.innerHTML += getChessPiece(pawn, whiteBlack)
+                    break
+
+                case piece === 'r':
+                    square.innerHTML += getChessPiece(rook, whiteBlack)
+                    break
+                case piece === 'n':
+                    square.innerHTML += getChessPiece(knight, whiteBlack)
+                    break
+                case piece === 'b':
+                    square.innerHTML += getChessPiece(bishop, whiteBlack)
+                    break
+                case piece === 'q':
+                    square.innerHTML += getChessPiece(queen, whiteBlack)
+                    break
+                case piece === 'k':
+                    square.innerHTML += getChessPiece(king, whiteBlack)
+                    break
+                case piece === 'p':
+                    square.innerHTML += getChessPiece(pawn, whiteBlack)
+                    break
+                case piece === ' ':
+                    square.innerHTML += ''
+                    break
+            }
 
             board.appendChild(square)
-        }
-    }
+        })
+    })
+}
+displayBoard()
 
-    // Place initial chess pieces on the board
-    placeInitialPieces(n)
+function handleDragDropEvents(board) {
+    let dragged
+
+    board.addEventListener('dragstart', (e) => {
+        dragged = e.target
+    })
+
+    board.addEventListener('dragover', (e) => {
+        e.preventDefault()
+    })
+
+    board.addEventListener('dragenter', (e) => {
+        if (e.target.classList.contains('square')) {
+            e.target.classList.add('bg-green-300')
+        }
+    })
+
+    board.addEventListener('dragleave', (e) => {
+        if (e.target.classList.contains('square')) {
+            e.target.classList.remove('bg-green-300')
+        }
+    })
+
+    board.addEventListener('drop', (e) => {
+        if (e.target.classList.contains('square')) {
+            e.target.classList.remove('bg-green-300')
+
+            e.target.appendChild(dragged)
+        }
+    })
 }
 
-// Create a grid square element
-function createSquares(x, y, n) {
-    const square = document.createElement('div')
-
-    square.classList.add('flex', 'justify-center', 'items-center')
-
-    board.style.gridTemplateColumns = `repeat(${n}, 1fr)`
-    board.style.gridTemplateRows = `repeat(${n}, 1fr)`
-
-    const drag = DragEvents(square)
-    drag.setDragEvents()
-
-    return square
+function getChessPiece(piece, type) {
+    return /* HTML */ `
+        <i
+            class="
+            w-full
+            h-full
+            cursor-grab
+            text-6xl
+            shadow-thin
+            grid
+            place-content-center
+            chessPiece
+            ${piece}
+            ${type}
+            "
+            draggable="true"
+        ></i>
+    `
 }
